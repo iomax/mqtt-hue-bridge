36a33258f653626bbd02d6371d4711b1c7eb62f5
iomax <massimo.romagnoli@gmail.com>
2017-01-29 13:21:46 +0100

    config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ config.debug = process.env.DEBUG || false;
 config.hue   = {};
 config.mqtt  = {};
 
-config.hue.hostname   = process.env.HUE_HOSTNAME   || 'hue';
-config.hue.username   = process.env.HUE_USERNAME   || '24a4f78b2b7af5ef24804893119a1377';
+config.hue.hostname   = process.env.HUE_HOSTNAME   || 'XXX';
+config.hue.username   = process.env.HUE_USERNAME   || 'YYYYYYYYYYYYYYYYYYYYYYYYY';
 
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || 'light';
 config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || 'localhost';
